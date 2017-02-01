fc3d165f97b64c0abf76c2372e4e3a609dbbd758
Nir Cohen <nir36g@gmail.com>
2017-02-01 11:12:10 +0200

    Up ver to 1.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(*parts):
 
 setup(
     name='repex',
-    version="1.1.0",
+    version="1.2.0",
     url='https://github.com/cloudify-cosmo/repex',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
